8b9e24c8231f0e134ed2bc50a4c5ab6c8598802d
karunsane <karunsane@gmail.com>
2020-05-20 10:07:43 -0500

    .35

---
+++ GestionInventario/class/query_consult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class query_consult
+{
+    static readonly string urI = @"URI=file:" + AppDomain.CurrentDomain.BaseDirectory + "/data.db";
+    SQLiteConnection sqlCon = new SQLiteConnection(urI);
+
+    public void query_data(string id)
+    {
+        SQLiteDataAdapter adp = new SQLiteDataAdapter("SELECT * FROM local WHERE id = " + id,sqlCon);
+        DataTable dt = new DataTable();
+        adp.Fill(dt);
+        
+    }
+}
+
